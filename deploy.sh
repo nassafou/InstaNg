3563eea9ba2ea0f183481b8823afe5e7e9be686a
nassafou <ntyoussouf@gmail.com>
2019-08-26 16:00:57 +0200

    [DEV]:mise en place de conditions

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,32 @@
 
 #################################################
 
+if [ "$1" == "--create" ];then
+ echo ""
+ echo "creation encours"
+ echo ""
+   elif [ "$1" == "--drop" ];then
+      echo ""
+      echo "Suppression en cours"
+      echo ""
+        elif [ "$1" == "--info" ];then
+           echo ""
+           echo "Info"
+           echo ""
+             elif [ "$1" == "--start" ];then
+                echo
+                echo "demarrer le conteneur "
+                echo ""  
+                  elif [ "$1" == "--ansible" ];then
+                  echo
+                  echo "Lancer Ansible"
+                  echo
 
+else
 echo "
 
 Options:
        - --create : lancer 
        - --drop : supprimer
 "
+fi
